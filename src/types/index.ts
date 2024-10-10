a59d91c40805e066bef611582dd52ecf9daee5c8
Olga M <ol.unya@yandex.ru>
2024-10-10 14:01:20 +0500

    добавлены конструктор, свойства и методы классов типов данных

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,18 +10,31 @@ export interface IItem {
 	image: string;
 	satus: string;
 }
-
+export type PayMethods = 'card' | 'cash';
 //интерфейс контактов пользователя
-interface IContact {
-	adress: string;
+interface IContacts {
 	email: string;
 	phone: string;
-	paymethod: string;
-	setEmail(email: string): void;
-	setPhone(phone: string): void;
-	clear(): void;
 }
+//интерфейс деталей заказа
 
+interface IOrderData{
+	adress: string;
+	paymethod: PayMethods;
+}
+//интерфейс проверки формы заказа
+export interface IFormValidation {
+	valid: boolean;
+	errors: Partial<Record<keyof IOrder, string>>;
+}
+//Интерфейс полученного заказа
+export interface IOrderResult {
+    total: number;
+}
+//Интерфейс действия с окном заказа
+interface IResultActions {
+    onClick: () => void;
+}
 //интерфейс каталога товаров
 export interface IItemsCatalog {
 	items: IItem[];
@@ -42,56 +55,37 @@ export interface IBasketModel {
 export interface IItemAPI {
 	getItems: () => Promise<IItem[]>;
 	getItem: (id: string) => Promise<IItem>;
-	orderItems: (order: IContact) => Promise<IOrderResult>;
+	orderItems: (order: IContacts) => Promise<IOrderResult>;
 }
 
 //типы для модальных окон: платеж и адрес, почта и телефон
-export type TPayInfo = Pick<IContact, 'adress' | 'paymethod'>;
-export type TOrderInfo = Pick<IContact, 'email' | 'phone'>;
-
-//Интерфейс для формы заказа
-export interface IOrderFormData {
-	contacts: IContact;
-	isActive: boolean;
-	isDisabled: boolean;
-	message: string;
-	total: string;
-	isError: boolean;
-}
-
-//Интерфейс для работы с настройками формы заказа
-
-export interface IOrderFormSettings {
-	onChange: (data: IContact) => void;
-	onClose: () => void;
-	onNext: () => void;
-	onBack: () => void;
-}
-//Интерфейс полученного заказа
-export interface IOrderResult {
-	id: string;
-}
+export type TPayInfo = Pick<IOrderData, 'adress' | 'paymethod'>;
+export type TOrderInfo = Pick<IContacts, 'email' | 'phone'>;
+export type IOrder = TPayInfo & TOrderInfo
 
 //Данные карточки товара для корзины
 type IBasketItem = Pick<IItem, 'name' | 'price'>;
 
-// Интерфейс состояния заполненной формы заказа
-interface IFormState {
-	valid: boolean;
-	errors: string[];
-}
 
 //VIEW
-
+// интерфейс действий с карточкой
+interface ICardActions {
+    onClick: (event: MouseEvent) => void;
+}
 // отображение для заданного типа данных
 
 interface IView {
 	render(data?: object): HTMLElement;
 }
-
+// отображение страницы с каталогом и счетчиком
+interface IPage {
+    counter: number;
+    catalog: HTMLElement[];
+    locked: boolean;
+}
 // отображение корзины
 interface IBasketView {
-	id: string;
+	selected: string[];
 	items: HTMLElement[];
 	total: number;
 }
@@ -100,7 +94,10 @@ interface IBasketView {
 interface IItemsContainer {
 	new (container: HTMLElement, events?: IEvents): IView;
 }
-
+//отображение модального окна
+interface IModalData {
+    content: HTMLElement;
+}
 //отображение для формы заказа
 interface IFormOrder {
 	new (container: HTMLElement, events?: IEvents): IView;
